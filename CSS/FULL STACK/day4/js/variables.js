14e928b21270184a00fbfeb837db3154092bb04d
Sahana-0114 <patilsahana2005@gmail.com>
2024-11-30 11:28:06 +0530

    Add files via upload

---
+++ CSS/FULL STACK/day4/js/variables.js
@@ -0,0 +1,9 @@
+let num1 = 40;
+let num2 = 20;
+let result = num1 - num2;
+
+document.getElementById('output').innerHTML =`
+<p>Number 1: ${num1}</P>
+<p>Number 2: ${num2}</P>
+<p>Sub: ${result}</p>
+`;
